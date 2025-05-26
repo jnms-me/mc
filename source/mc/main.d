d4579a50c7e18de93df0b25939f198878965a572
Jonas Meeuws <jonas.meeuws@gmail.com>
2025-05-26 15:21:58 +0200

    Add memoryerror.d from dmd master

--- source/mc/main.d
+++ source/mc/main.d
@@ -1,13 +1,22 @@
 module mc.main;
 
+import etc.linux.memoryerror : registerMemoryAssertHandler;
+
 import vibe.core.core : runEventLoopOnce;
 
-import mc.util.log : Logger;
 import mc.server.server : runServerTask;
+import mc.util.log : Logger;
 
 @safe:
 
 immutable log = Logger.moduleLogger;
+
+
+@trusted shared static
+this()
+{
+    registerMemoryAssertHandler;
+}
 
 void main()
 {
